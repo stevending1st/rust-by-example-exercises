2d153f82eddad0f2b7542868c03cdaddf21cb8e8
stevending1st <stevending1st@163.com>
2023-04-22 20:44:32 +0800

    feat: add main.rs, print_display.rs, print.rs

---
+++ exercises/hello/print.rs
@@ -0,0 +1,46 @@
+// 改正上面代码中的两个错误（见 “改正”），使它可以没有错误地运行。
+// 再用一个 println! 宏，通过控制显示的小数位数来打印：Pi is roughly 3.142（Pi 约等于 3.142）。为了达到练习目的，使用 let pi = 3.141592 作为 Pi 的近似值（提示：设置小数位的显示格式可以参考文档 std::fmt）。
+
+fn main() {
+    // 通常情况下，`{}` 会被任意变量内容所替换。
+    // 变量内容会转化成字符串。
+    println!("{} days", 31);
+
+    // 不加后缀的话，31 就自动成为 i32 类型。
+    // 你可以添加后缀来改变 31 的类型（例如使用 31i64 声明 31 为 i64 类型）。
+
+    // 用变量替换字符串有多种写法。
+    // 比如可以使用位置参数。
+    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+
+    // 可以使用命名参数。
+    println!("{subject} {verb} {object}",
+             object="the lazy dog",
+             subject="the quick brown fox",
+             verb="jumps over");
+
+    // 可以在 `:` 后面指定特殊的格式。
+    println!("{} of {:b} people know binary, the other half don't", 1, 2);
+
+    // 你可以按指定宽度来右对齐文本。
+    // 下面语句输出 "     1"，5 个空格后面连着 1。
+    println!("{number:>width$}", number=1, width=6);
+
+    // 你可以在数字左边补 0。下面语句输出 "000001"。
+    println!("{number:>0width$}", number=1, width=6);
+
+    // println! 会检查使用到的参数数量是否正确。
+    println!("My name is {0}, {1} {0}", "Bond", "James");
+    // 改正 ^ 补上漏掉的参数："James"
+
+    // 创建一个包含单个 `i32` 的结构体（structure）。命名为 `Structure`。
+    #[allow(dead_code)]
+    struct Structure(i32);
+
+    // 但是像结构体这样的自定义类型需要更复杂的方式来处理。
+    // 下面语句无法运行。
+    // println!("This struct `{}` won't print...", Structure(3));
+    // 改正 ^ 注释掉此行。
+    
+    println!("Pi is roughly {pi:>.3}", pi = 3.141592)
+}
